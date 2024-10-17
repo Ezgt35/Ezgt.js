2e64b684faeab12aa881daf9d337efc4957c3ba5
Ezgt35 <bangezgt@gmail.com>
2024-10-17 12:38:58 +0700

    Create Ezgt.js

---
+++ Ezgt.js
@@ -0,0 +1,22 @@
+{
+  "users": [
+    {
+      "whatsappNumber": "6283840856644",
+      "password": "ezgt"
+    },
+    {
+      "whatsappNumber": "083846147781",
+      "password": "123"
+    }
+  ],
+  "approvedUsers": [
+    {
+      "whatsappNumber": "6283840856644",
+      "token": "ezgt123"
+    },
+    {
+      "whatsappNumber": "083846147781",
+      "token": "rafael123"
+    }
+  ]
+}
