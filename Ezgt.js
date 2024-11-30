49805d830429eb1b16f6518f1daac5ce32fb9bcd
Ezgt35 <bangezgt@gmail.com>
2024-11-30 20:34:51 +0700

    Update Ezgt.js

--- Ezgt.js
+++ Ezgt.js
@@ -1,22 +1,11 @@
 {
-  "users": [
+  "keys": [
     {
-      "whatsappNumber": "62895425873232",
-      "password": "ezgt"
-    },
-    {
-      "whatsappNumber": "083846147781",
-      "password": "123"
-    }
-  ],
-  "approvedUsers": [
-    {
-      "whatsappNumber": "62895425873232",
-      "token": "ezgt123"
-    },
-    {
-      "whatsappNumber": "083846147781",
-      "token": "rafael123"
+      "key": "ezgt",
+      "numbers": [
+        "6288802747938",
+        "62895425873232"
+      ]
     }
   ]
 }
